f1f48dcd7b50a2f6716e0b314a8204bf79a75830
fairy-scary <ecoautumn@gmail.com>
2020-11-19 11:13:12 -0600

    server prob

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require("express");
-
+const morgan = require("morgan");
 const routes = require("./routes");
 
 const app = express();
 
 app.set("view engine", "pug");
 
+app.use(morgan('dev'));
 app.use(routes);
-
-// Define a port and start listening for connections.
 
 const port = 8080;
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+// Define a port and start listening for connections.
+
+module.exports = app;
