18b1c516e350949182ea70d177e7f98796baa341
Andrew Dotterer <adotterer@gmail.com>
2020-11-19 10:32:45 -0800

    part 1 complete

--- app.js
+++ app.js
@@ -9,9 +9,49 @@ app.set("view engine", "pug");
 app.use(morgan('dev'));
 app.use(routes);
 
-const port = 8080;
+app.use((req, res, next) => {
+  const err = new Error("The requested page couldn't be found.");
+  err.status = 404;
+  next(err);
+});
 
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+// Error handler to log errors.
+app.use((err, req, res, next) => {
+  if (process.env.NODE_ENV === "production") {
+    // TODO Log the error to the database.
+  } else {
+    console.error(err);
+  }
+  next(err);
+});
+
+// Error handler for 404 errors.
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404);
+    res.render('page-not-found', {
+      title: 'Page Not Found',
+    });
+  } else {
+    next(err);
+  }
+});
+
+// Generic error handler.
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  const isProduction = process.env.NODE_ENV === 'production';
+  res.render('error', {
+    title: 'Server Error',
+    message: isProduction ? null : err.message,
+    stack: isProduction ? null : err.stack,
+  });
+});
+
+
+// const port = 8080;
+
+// app.listen(port, () => console.log(`Listening on port ${port}...`));
 // Define a port and start listening for connections.
 
 module.exports = app;
